9666be874293af9faacaca3fc572129049c8e7b8
Diwanshu <kumardiwanshu00@gmail.com>
2024-06-20 16:12:14 +0530

    refactoring : no major changes only refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
 import './App.css';
 import { Navbar } from './Layout/HeaderAndFooter/Navbar';
-import { ExploreTopBooks } from './Layout/Homepage/ExploreTopBooks';
-import { Carousel } from './Layout/Homepage/Carousel';
-import { Heros } from './Layout/Homepage/Heros';
-import { LibrrayServices } from './Layout/Homepage/LibraryServices';
 import { Footer } from './Layout/HeaderAndFooter/Footer';
+import { Homepage } from './Layout/Homepage/Homepage';
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
-      <LibrrayServices/>
+      <Homepage/>
       <Footer/>
     </div>
   );
 }
 
-export default App;
