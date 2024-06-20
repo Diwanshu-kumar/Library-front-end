a1a8f12ea9aa780317e23739c523f4f86a8bb1f8
Diwanshu <kumardiwanshu00@gmail.com>
2024-06-20 15:36:21 +0530

    Added : Carousel ExploreBooks Heros and ReturnBook Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Navbar } from './Layout/HeaderAndFooter/Navbar';
+import { ExploreTopBooks } from './Layout/Homepage/ExploreTopBooks';
+import { Carousel } from './Layout/Homepage/Carousel';
+import { Heros } from './Layout/Homepage/Heros';
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+      <Carousel/>
+      <Heros/>
+    </div>
   );
 }
 
